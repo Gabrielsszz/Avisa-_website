dbc2c470bc31405b3c8a0521195a6edf7541598b
Iago de Moura Sousa <iagomoura640@gmail.com>
2023-06-06 15:49:50 -0300

    new update

--- src/script.js
+++ src/script.js
@@ -1,18 +1,42 @@
-const inputs = document.querySelectorAll('.input');
+// Seletor dos campos de email e senha
+const emailInput = document.querySelector('input[name="email"]');
+const passwordInput = document.querySelector('input[name="password"]');
 const button = document.querySelector('.login__button');
 
+// Função de tratamento de evento para o botão de login
+button.addEventListener('click', () => {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Autenticação bem-sucedida, obtenha o objeto do usuário
+      var user = userCredential.user;
+      console.log(user);
+      // Redirecione ou realize outras ações após o login bem-sucedido
+    })
+    .catch((error) => {
+      // Tratamento de erros
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+});
+
+// Funções handleFocus, handleFocusOut e handleChange (sem alterações)
+const inputs = document.querySelectorAll('.input');
 
 const handleFocus = ({ target }) => {
   const span = target.previousElementSibling;
   span.classList.add('span-active');
-}
+};
 
 const handleFocusOut = ({ target }) => {
   if (target.value === '') {
     const span = target.previousElementSibling;
     span.classList.remove('span-active');
   }
-}
+};
 
 const handleChange = () => {
   const [username, password] = inputs;
@@ -22,22 +46,8 @@ const handleChange = () => {
   } else {
     button.setAttribute('disabled', '');
   }
-}
+};
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
 inputs.forEach((input) => input.addEventListener('input', handleChange));
-
-// Autenticação com email e senha
-firebase.auth().signInWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Autenticação bem-sucedida, obtenha o objeto do usuário
-    var user = userCredential.user;
-    console.log(user);
-  })
-  .catch((error) => {
-    // Tratamento de erros
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode, errorMessage);
-  });
