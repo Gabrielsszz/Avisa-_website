6f13ebfa6404f9f0f81bea2fd558b48c9a760a95
Iago de Moura Sousa <iagomoura640@gmail.com>
2023-06-06 19:11:27 -0300

    new comit update

--- src/inicio.js
+++ src/inicio.js
@@ -1,36 +1,21 @@
-const slider = document.querySelectorAll('.slider');
-const btnPrev = document.getElementById('prev-button');
-const btnNext = document.getElementById('next-button');
 
-let currentSlide = 0;
+    $(document).ready(function() {
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                }
+            }
+        });
+    });
 
-function hideSlider() {
-  slider.forEach(item => item.classList.remove('on'))
-}
-
-function showSlider() {
-  slider[currentSlide].classList.add('on')
-}
-
-function nextSlider() {
-  hideSlider()
-  if(currentSlide === slider.length -1) {
-    currentSlide = 0
-  } else {
-    currentSlide++
-  }
-  showSlider()
-}
-
-function prevSlider() {
-  hideSlider()
-  if(currentSlide === 0) {
-    currentSlide = slider.length -1
-  } else {
-    currentSlide--
-  }
-  showSlider()
-}
-
-btnNext.addEventListener('click', nextSlider)
-btnPrev.addEventListener('click', prevSlider)
