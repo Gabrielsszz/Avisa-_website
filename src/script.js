2dabd5bad6c18158f07a5d9a437adc982a53692c
Iago de Moura Sousa <iagomoura640@gmail.com>
2023-06-10 16:48:15 -0300

    landingpg

--- src/script.js
+++ src/script.js
@@ -3,25 +3,6 @@ const emailInput = document.querySelector('input[name="email"]');
 const passwordInput = document.querySelector('input[name="password"]');
 const button = document.querySelector('.login__button');
 
-// Função de tratamento de evento para o botão de login
-button.addEventListener('click', () => {
-  const email = emailInput.value;
-  const password = passwordInput.value;
-
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Autenticação bem-sucedida, obtenha o objeto do usuário
-      var user = userCredential.user;
-      console.log(user);
-      // Redirecione ou realize outras ações após o login bem-sucedido
-    })
-    .catch((error) => {
-      // Tratamento de erros
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode, errorMessage);
-    });
-});
 
 // Funções handleFocus, handleFocusOut e handleChange (sem alterações)
 const inputs = document.querySelectorAll('.input');
@@ -39,7 +20,7 @@ const handleFocusOut = ({ target }) => {
 };
 
 const handleChange = () => {
-  const [username, password] = inputs;
+  const [email, username, password] = inputs;
 
   if (username.value && password.value.length >= 8) {
     button.removeAttribute('disabled');
