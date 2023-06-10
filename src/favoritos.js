8041c4b981323535da8fa61d33148a276a7a01a6
Iago de Moura Sousa <iagomoura640@gmail.com>
2023-06-10 14:12:51 -0300

    comit11111

--- src/favoritos.js
+++ src/favoritos.js
@@ -54,3 +54,8 @@ const product = [
   }
   
   }
+  function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav-hamb").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
