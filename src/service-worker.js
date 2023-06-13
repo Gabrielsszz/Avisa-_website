424dee192ca7b6b3a27cce8a09c826d93fa6b021
Iago de Moura Sousa <iagomoura640@gmail.com>
2023-06-13 16:53:05 -0300

    pwa teste

---
+++ src/service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          'CSS/landingpg.css',
+          'src/landingpg.js',
+          'images/image.png'
+          // Adicione aqui todos os recursos estáticos que deseja armazenar em cache
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
