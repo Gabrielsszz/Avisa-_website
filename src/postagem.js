a059c5410763c1f5762b425891d88c339fced241
iago moura <iagomoura640@gmail.com>
2023-06-14 18:32:56 -0300

    comit post

---
+++ src/postagem.js
@@ -0,0 +1,6 @@
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav-hamb").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+    document.body.classList.toggle("fixed-menu");
+  }
